5d189c24c1f6c878958ceafcb23cf4b39da08cc6
github username <github email address>
2020-04-06 14:06:36 -0400

    Final

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -10,4 +10,12 @@ class Ingredient
     def self.all
         @@all
     end
+
+    def self.most_common_allergen
+        self.all.max { |ingredient1, ingredient2| Ingredient.num_users(ingredient1) <=> Ingredient.num_users(ingredient2) }
+    end
+
+    def self.num_users(ingredient)
+        Allergy.all.select { |allergy| allergy.ingredient == ingredient }.length
+    end
 end
--- app/models/RecipeCard.rb
+++ app/models/RecipeCard.rb
@@ -2,7 +2,7 @@ class RecipeCard
     attr_accessor :user, :recipe, :date, :rating
     @@all = []
 
-    def initialize(user, recipe, date=Time.now.to_s, rating="no rating")
+    def initialize(user, recipe, date=Time.now.to_s, rating=0)
         @user = user
         @recipe = recipe
         @date = date
